293ab05a0aae135e03b49c7de68bbcd9892aac99
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-15 13:02:47 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -9,5 +10,14 @@ public class Main {
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         System.out.println("ФИО сотрудника - " + lastName + " " + firstName + " " + middleName);
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName.toUpperCase());
     }
 }
